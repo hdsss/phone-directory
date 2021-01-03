fc6d16396eea1c3d978caee2ba35e67aae16912e
hsisodi <hridesh.sisodia@daimler.com>
2021-01-03 16:37:27 +0530

    Created skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
-function App() {
-    return (
-        <div >
-            Phone Directory
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="header">Phone Directory</div>
+                <div>
+                    <span>Name</span><br />
+                    <span>Phone</span>
+                </div>
+                <button>Add</button>
+                <label htmlFor="name">Name: </label>
+                <input id="name" type="text" placeholder="Type Here" defaultValue="shilpa"/>
+            </div>
+        );
+    }
+
 }
 
 export default App;

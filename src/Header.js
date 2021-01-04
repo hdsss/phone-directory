0d662a276d5398d489d0bff6b7eb4c4fc9a0a26b
hsisodi <hridesh.sisodia@daimler.com>
2021-01-04 20:23:48 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Component } from "react"
 
+const headerStyle = { textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase' };
+
 const Header = function () {
-    return(
-    <div className="header">Phone Directory</div>
+    return (
+        <div style={headerStyle}>Phone Directory</div>
     )
 }
 
@@ -11,6 +13,17 @@ const Header = function () {
 //     render() {
 //         return(
 //         <div className="header">Phone Directory</div>
+//         )
+//     }
+// }
+
+// class App extends Component {
+//     render() {
+//         return(
+//         <div className="App">
+//             <p>Upgrad - Building Career of tommorow!</p>
+//             <App/>
+//             </div>
 //         )
 //     }
 // }
